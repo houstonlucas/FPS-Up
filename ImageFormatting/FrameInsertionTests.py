b464d8e9541d036fb2229df89a65019e2ba1e81c
Houston Lucas <houstonlucas@nevada.unr.edu>
2017-09-30 14:44:01 -0700

    Function to get frames of a video.

---
+++ ImageFormatting/FrameInsertionTests.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+def main():
+    frames = getVideoFrames("small.mp4")
+    print(len(frames))
+
+def getVideoFrames(file_name):
+    cap = cv2.VideoCapture(file_name)
+
+    frames = []
+
+    r, frame = cap.read()
+    while r:
+        frames.append(frame)
+        r, frame = cap.read()
+
+    return frames
+
+
+if __name__ == "__main__":
+    main()
