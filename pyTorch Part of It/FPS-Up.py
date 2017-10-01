87b8e688f04ba1db71e855587fcfd51735405555
cmiley <cmiley@nevada.unr.edu>
2017-10-01 13:59:28 -0700

    Updated img processing

--- pyTorch Part of It/FPS-Up.py
+++ pyTorch Part of It/FPS-Up.py
@@ -19,16 +19,28 @@ def main():
 
     img1 = frames[0]
     img2 = frames[2]
+    #print(img1)
 
     imgs = np.stack((img1, img2), 0)
+    #print(imgs)
     print(imgs.dtype)
 
+    inp1 = Variable(torch.from_numpy(img1).type(dtype))
+    # inp1.byte()
+    inp2 = Variable(torch.from_numpy(img2).type(dtype))
+    # inp2.byte()
+
+    inps = (inp1, inp2)
+
     inputs = Variable(torch.from_numpy(imgs).type(dtype))
-    outputs = fup(inputs)
+    print(inputs.size())
+    outputs = fup.forward(inps)
     print(outputs.size())
     img_out = outputs.data.numpy()[0].reshape(h, w, 3)
     print(img_out.shape, img1.shape)
     cv2.imshow("Output", img_out)
+    cv2.imshow("Original", img1)
+    cv2.imshow("Original - Output", inp1.data.numpy())
     cv2.waitKey(-1)
 
 
@@ -47,14 +59,14 @@ class FPS_UP(nn.Module):
         self.conv2 = nn.Conv2d(3, self.k, self.d)
 
         # TODO learn about padding
-        self.deConv = nn.ConvTranspose2d(2 * self.k, 3, self.d)
+        self.deConv = nn.ConvTranspose2d(self.k, 3, self.d)
 
     def forward(self, imgs):
         img1 = imgs[0].view(1, 3, self.img_height, self.img_width)
         img2 = imgs[1].view(1, 3, self.img_height, self.img_width)
         c1 = self.conv1(img1)
         c2 = self.conv2(img2)
-        a = torch.cat((c1, c2), 1)
+        a = torch.cat((c1, c2), 0)
         img_out = self.deConv(a)
         return img_out
 
