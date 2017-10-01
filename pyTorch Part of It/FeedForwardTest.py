acf22d8d8f55a2a01376119d564fb11b8be33426
houstonlucas <houstonlucas@nevada.unr.edu>
2017-09-30 22:20:55 -0700

    Modified FeedForward to work with cuda.

--- pyTorch Part of It/FeedForwardTest.py
+++ pyTorch Part of It/FeedForwardTest.py
@@ -12,10 +12,13 @@ import time
 
 
 def main():
+    start_time = time.time()
+
     input_size = 2
     output_size = 1
+    gpu_enabled = True
 
-    num_examples = 500
+    num_examples = 5000
     num_train = num_examples / 2
     num_epochs = 100000
 
@@ -30,13 +33,20 @@ def main():
 
     net = Net()
 
+    dtype = torch.FloatTensor
+    if gpu_enabled:
+        net.cuda()
+        dtype = torch.cuda.FloatTensor
+
     loss_fun = nn.MSELoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.0001)
 
+    # load data
+    inputs = Variable(torch.from_numpy(x_train).type(dtype))
+    targets = Variable(torch.from_numpy(y_train).type(dtype))
+
     # train
     for epoch in range(num_epochs):
-        inputs = Variable(torch.from_numpy(x_train).float())
-        targets = Variable(torch.from_numpy(y_train).float())
 
         optimizer.zero_grad()
         outputs = net(inputs)
@@ -45,39 +55,53 @@ def main():
         optimizer.step()
 
         # Status Report
-        if (epoch + 1) % 2000 == 0:
+        if (epoch + 1) % int(num_epochs*0.1) == 0:
             print('Epoch [%d/%d]\n\tTraining Loss: %.4f'
                   % (epoch + 1, num_epochs, loss.data[0]))
 
             # Test data
-            inputs = Variable(torch.from_numpy(x_test).float())
+            inputs = Variable(torch.from_numpy(x_test).type(dtype))
             outputs = net(inputs)
-            targets = Variable(torch.from_numpy(y_test).float())
+            targets = Variable(torch.from_numpy(y_test).type(dtype))
             loss = loss_fun(outputs, targets)
             print("\tTesting Loss: %.4f" % (loss.data[0]))
 
+            # Reload train data
+            inputs = Variable(torch.from_numpy(x_train).type(dtype))
+            targets = Variable(torch.from_numpy(y_train).type(dtype))
+
+    num_to_visualize = 100
+
     # Visualize results
+    print("Took {} seconds".format(time.time() - start_time))
 
     # Actual data
-    inputs = Variable(torch.from_numpy(x_train).float())
+    inputs = Variable(torch.from_numpy(x_train[:num_to_visualize]).type(dtype))
     outputs = net(inputs)
-    targets = Variable(torch.from_numpy(y_train).float())
+    targets = Variable(torch.from_numpy(y_train[:num_to_visualize]).type(dtype))
     loss = loss_fun(outputs, targets)
     print("Training Loss: %.4f" % (loss.data[0]))
 
-    ax = prepare_scatter_plot(x_train, y_train)
+    ax = prepare_scatter_plot(x_train[:num_to_visualize], y_train[:num_to_visualize])
 
-    ax = prepare_scatter_plot(x_train, outputs.data.numpy(), '^', 'g', ax)
+    if gpu_enabled:
+        data = outputs.cpu().data.numpy()
+    else:
+        data = outputs.data.numpy()
+    ax = prepare_scatter_plot(x_train[:num_to_visualize], data, '^', 'g', ax)
 
     # Train data
-    inputs = Variable(torch.from_numpy(x_test).float())
+    inputs = Variable(torch.from_numpy(x_test[:num_to_visualize]).type(dtype))
     outputs = net(inputs)
-    targets = Variable(torch.from_numpy(y_test).float())
+    targets = Variable(torch.from_numpy(y_test[:num_to_visualize]).type(dtype))
     loss = loss_fun(outputs, targets)
     print("Testing Loss: %.4f" % (loss.data[0]))
 
-    # print(type(outputs))
-    prepare_scatter_plot(x_train, outputs.data.numpy(), 'x', 'r', ax)
+    if gpu_enabled:
+        data = outputs.cpu().data.numpy()
+    else:
+        data = outputs.data.numpy()
+    prepare_scatter_plot(x_train[:num_to_visualize], data, 'x', 'r', ax)
 
     plt.show()
 
@@ -90,7 +114,7 @@ def toy_data(x, n, output_size):
     return y
 
 
-def prepare_scatter_plot(input_values, output_values, mark='o', color='b', ax = None):
+def prepare_scatter_plot(input_values, output_values, mark='o', color='b', ax=None):
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
